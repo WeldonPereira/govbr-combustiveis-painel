48deb2941ab50312a230e08c530bd998451fa79a
Weldon <weldonbarros179@gmail.com>
2025-12-16 09:18:32 -0300

    feat: configure application providers and routing structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { GovLayout } from './layout/gov-layout/gov-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: GovLayout,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/placeholder/placeholder')
+            .then(m => m.PlaceholderComponent)
+      }
+    ]
+  }
+];
