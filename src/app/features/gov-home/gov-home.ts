3805e7de2003417dfd43b5fd301d4cb6616dcd74
Weldon <weldonbarros179@gmail.com>
2025-12-18 14:06:53 -0300

    feat(gov-home): add institutional home page layout

---
+++ src/app/features/gov-home/gov-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-gov-home',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './gov-home.html',
+  styleUrls: ['./gov-home.scss'],
+})
+export class GovHomeComponent {}
