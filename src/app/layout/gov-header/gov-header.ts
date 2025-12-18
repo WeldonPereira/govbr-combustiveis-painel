2cb7fd53e388d336ae90142ad3aefb439c7355b3
Weldon <weldonbarros179@gmail.com>
2025-12-18 14:07:26 -0300

    feat(header): update gov.br logo colors and header layout

--- src/app/layout/gov-header/gov-header.ts
+++ src/app/layout/gov-header/gov-header.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GovMenuComponent } from '../../shared/gov-menu/gov-menu';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-gov-header',
   standalone: true,
-  imports: [GovMenuComponent],
+  imports: [GovMenuComponent, RouterLink, RouterLinkActive],
   templateUrl: './gov-header.html',
   styleUrls: ['./gov-header.scss'],
 })
