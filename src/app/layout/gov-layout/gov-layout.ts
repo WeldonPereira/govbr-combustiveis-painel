ea0679f720d9b2a53b406abdc770f069b301adbe
Weldon <weldonbarros179@gmail.com>
2025-12-20 17:15:59 -0300

    style: improve layout structure

--- src/app/layout/gov-layout/gov-layout.ts
+++ src/app/layout/gov-layout/gov-layout.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GovHeaderComponent } from '../gov-header/gov-header';
-import { BreadcrumbComponent } from '../../shared/breadcrumb/breadcrumb';
 
 @Component({
   selector: 'app-gov-layout',
   standalone: true,
-  imports: [RouterOutlet, GovHeaderComponent, BreadcrumbComponent],
+  imports: [RouterOutlet],
   templateUrl: './gov-layout.html',
   styleUrls: ['./gov-layout.scss'],
 })
